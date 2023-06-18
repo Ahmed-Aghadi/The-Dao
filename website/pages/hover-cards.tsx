9fe34dbd77bf0d6a9df76713fb5a6a3acda0fd09
ahmedaghadi <ahmedaghadi123@gmail.com>
2023-06-18 18:44:09 +0530

    remove test.ts

--- website/pages/hover-cards.tsx
+++ website/pages/hover-cards.tsx
@@ -1,30 +1,21 @@
 import { HoverCards } from "@/components/HoverCards";
 
 export default function Home() {
-    const divs = Array.from(Array(12)).map((_, index) => {
-        return (
-            <div
-                key={index}
-                style={{
-                    color: "rgb(240, 240, 240)",
-                }}
-            >
-                {index}
-            </div>
-        );
-    });
+  const divs = Array.from(Array(12)).map((_, index) => {
     return (
-        <div
-            style={
-                {
-                    // backgroundColor: "rgb(20, 20, 20)",
-                    // backgroundColor: "black",
-                    // height: "100vh",
-                    // width: "100vw",
-                }
-            }
-        >
-            <HoverCards divs={divs} />
-        </div>
+      <div
+        key={index}
+        style={{
+          color: "rgb(240, 240, 240)",
+        }}
+      >
+        {index}
+      </div>
     );
+  });
+  return (
+    <div>
+      <HoverCards divs={divs} />
+    </div>
+  );
 }
